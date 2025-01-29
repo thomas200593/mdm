042a2479bafcd35c283149fdb6026b225ba3a650
thomas200593 <thomas200593@yahoo.com>
2025-01-29 13:34:49 +0700

    U Update Libraries

--- app/src/main/java/com/thomas200593/mdm/app/App.kt
+++ app/src/main/java/com/thomas200593/mdm/app/App.kt
@@ -1,5 +1,7 @@
 package com.thomas200593.mdm.app
 
 import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App: MultiDexApplication()
--- app/src/main/java/com/thomas200593/mdm/app/main/ActMain.kt
+++ app/src/main/java/com/thomas200593/mdm/app/main/ActMain.kt
@@ -1,5 +1,7 @@
 package com.thomas200593.mdm.app.main
 
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ActMain: AppCompatActivity()
