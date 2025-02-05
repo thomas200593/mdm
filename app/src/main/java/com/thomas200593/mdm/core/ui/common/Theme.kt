55b9c7db19a2c85e661e9d978aa93494240d9b4e
thomas200593 <thomas200593@yahoo.com>
2025-02-06 01:24:34 +0700

    U Color.kt  U Theme.kt

--- app/src/main/java/com/thomas200593/mdm/core/ui/common/Theme.kt
+++ app/src/main/java/com/thomas200593/mdm/core/ui/common/Theme.kt
@@ -4,4 +4,7 @@ object Theme {
     private val schemeLightContrastDefault = Color.Light.schemeContrastDefault
     private val schemeLightContrastMedium = Color.Light.schemeContrastMedium
     private val schemeLightContrastHigh = Color.Light.schemeContrastHigh
+    private val schemeDarkContrastDefault = Color.Dark.schemeContrastDefault
+    private val schemeDarkContrastMedium = Color.Dark.schemeContrastMedium
+    private val schemeDarkContrastHigh = Color.Dark.schemeContrastHigh
 }
