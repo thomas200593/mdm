fe9b5de64675cab6bfc23e70051ecf8a79dfeff5
thomas200593 <thomas200593@yahoo.com>
2025-02-06 22:55:58 +0700

    U Color.kt  U Theme.kt

--- app/src/main/java/com/thomas200593/mdm/core/ui/common/Theme.kt
+++ app/src/main/java/com/thomas200593/mdm/core/ui/common/Theme.kt
@@ -1,10 +1,10 @@
 package com.thomas200593.mdm.core.ui.common
 
 object Theme {
-    private val schemeLightContrastDefault = Color.Light.schemeContrastDefault
-    private val schemeLightContrastMedium = Color.Light.schemeContrastMedium
-    private val schemeLightContrastHigh = Color.Light.schemeContrastHigh
-    private val schemeDarkContrastDefault = Color.Dark.schemeContrastDefault
-    private val schemeDarkContrastMedium = Color.Dark.schemeContrastMedium
-    private val schemeDarkContrastHigh = Color.Dark.schemeContrastHigh
+    private val lightContrastDefault = Color.Light.ContrastDefault.colorScheme
+    private val lightContrastMedium = Color.Light.ContrastMedium.colorScheme
+    private val lightContrastHigh = Color.Light.ContrastHigh.colorScheme
+    private val darkContrastDefault = Color.Dark.ContrastDefault.colorScheme
+    private val darkContrastMedium = Color.Dark.ContrastMedium.colorScheme
+    private val darkContrastHigh = Color.Dark.ContrastHigh.colorScheme
 }
