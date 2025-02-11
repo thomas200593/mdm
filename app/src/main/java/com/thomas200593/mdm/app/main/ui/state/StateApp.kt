0d22a1b45a91fff8493a91efcdb47ca4fff25da7
thomas200593 <thomas200593@yahoo.com>
2025-02-11 17:22:36 +0700

    A Font set  U Add class definition

--- app/src/main/java/com/thomas200593/mdm/app/main/ui/state/StateApp.kt
+++ app/src/main/java/com/thomas200593/mdm/app/main/ui/state/StateApp.kt
@@ -3,12 +3,30 @@ package com.thomas200593.mdm.app.main.ui.state
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
-fun rememberStateApp() : StateApp =
-    remember {
-        StateApp()
+fun rememberStateApp(
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    navController: NavHostController = rememberNavController()
+) : StateApp =
+    remember(
+        coroutineScope,
+        navController
+    ) {
+        StateApp(
+            coroutineScope = coroutineScope,
+            navController = navController
+        )
     }
 
 @Stable
-class StateApp
+class StateApp(
+    coroutineScope: CoroutineScope,
+    navController: NavHostController
+) {
+
+}
