141986ae5d3ef2ca683cdb370f62e427c4767381
thomas200593 <thomas200593@yahoo.com>
2025-02-13 19:08:42 +0700

    A Screen Application

--- app/src/main/java/com/thomas200593/mdm/app/main/ui/ScrApp.kt
+++ app/src/main/java/com/thomas200593/mdm/app/main/ui/ScrApp.kt
@@ -1,12 +1,56 @@
 package com.thomas200593.mdm.app.main.ui
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.adaptive.WindowAdaptiveInfo
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.thomas200593.mdm.R
 import com.thomas200593.mdm.core.design_system.state_app.LocalStateApp
 import com.thomas200593.mdm.core.design_system.state_app.StateApp
+import com.thomas200593.mdm.core.ui.component.AppNavSuiteScaffold
 
 @Composable
-fun ScrApp(stateApp: StateApp = LocalStateApp.current) {
+fun ScrApp(
+    stateApp: StateApp = LocalStateApp.current,
+    windowAdaptiveInfo: WindowAdaptiveInfo = currentWindowAdaptiveInfo()
+) {
+    val snackBarHostState = remember { SnackbarHostState() }
     val isNetworkOffline by stateApp.isNetworkOffline.collectAsStateWithLifecycle()
+    val strNetworkOffline = stringResource(R.string.app_name)
+
+    LaunchedEffect(isNetworkOffline) {
+        if(isNetworkOffline) snackBarHostState.showSnackbar(
+            message = strNetworkOffline,
+            duration = SnackbarDuration.Indefinite
+        )
+    }
+
+    ScrApp(
+        stateApp = stateApp,
+        windowAdaptiveInfo = windowAdaptiveInfo,
+        snackBarHostState = snackBarHostState
+    )
+}
+
+@Composable
+private fun ScrApp(
+    modifier: Modifier = Modifier,
+    stateApp: StateApp,
+    windowAdaptiveInfo: WindowAdaptiveInfo = currentWindowAdaptiveInfo(),
+    snackBarHostState: SnackbarHostState,
+) {
+    AppNavSuiteScaffold(
+        modifier = modifier,
+        navSuiteItems = {},
+        windowAdaptiveInfo = windowAdaptiveInfo,
+        content = { /*TODO*/ }
+    )
 }
