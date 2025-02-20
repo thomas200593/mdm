ff015364d139b4aa3ac33fb31685bf77713bfc3e
thomas200593 <thomas200593@yahoo.com>
2025-02-20 15:38:45 +0700

    A App State

--- app/src/main/java/com/thomas200593/mdm/app/main/nav/DestTopLevel.kt
+++ app/src/main/java/com/thomas200593/mdm/app/main/nav/DestTopLevel.kt
@@ -1,19 +1,6 @@
 package com.thomas200593.mdm.app.main.nav
 
-import kotlin.reflect.KClass
-
-enum class DestTopLevel(
-    val scrGraphs: ScrGraphs,
-    route: KClass<*>,
-    baseRoute: KClass<*> = route
-) {
-    DASHBOARD(
-        scrGraphs = ScrGraphs.DestTopLevel.Dashboard,
-        route = ScrGraphs.DestTopLevel.Dashboard::class,
-        baseRoute = ScrGraphs.DestTopLevel.Dashboard::class
-    ),
-    PROFILE(
-        scrGraphs = ScrGraphs.DestTopLevel.UserProfile,
-        route = ScrGraphs.DestTopLevel.UserProfile::class
-    )
+enum class DestTopLevel{
+    DASHBOARD,
+    PROFILE
 }
