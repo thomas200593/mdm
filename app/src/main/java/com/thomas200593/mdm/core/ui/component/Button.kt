fd5b97c9a3180729d13c9fee400bc6c0a7eb455c
thomas200593 <thomas200593@yahoo.com>
2025-02-27 12:51:15 +0700

    A Button Language

--- app/src/main/java/com/thomas200593/mdm/core/ui/component/Button.kt
+++ app/src/main/java/com/thomas200593/mdm/core/ui/component/Button.kt
@@ -3,26 +3,20 @@ package com.thomas200593.mdm.core.ui.component
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ButtonElevation
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.thomas200593.mdm.core.design_system.util.Constants
-import com.thomas200593.mdm.core.ui.common.Theme
-import com.thomas200593.mdm.features.conf.__contrast_accent.entity.ContrastAccent
-import com.thomas200593.mdm.features.conf.__font_size.entity.FontSize
 
 @Composable
 fun BtnConfLang(
@@ -32,9 +26,9 @@ fun BtnConfLang(
     languageName: String? = null,
     enabled: Boolean = true,
     shape: Shape = MaterialTheme.shapes.medium,
-    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
     elevation: ButtonElevation? = null,
-    border: BorderStroke? = null,
+    border: BorderStroke? = BorderStroke(1.dp, MaterialTheme.colorScheme.onSurface),
     contentPadding: PaddingValues = ButtonDefaults. TextButtonContentPadding,
     interactionSource: MutableInteractionSource? = null
 ) {
@@ -53,29 +47,7 @@ fun BtnConfLang(
                 modifier = Modifier,
                 horizontalArrangement = Arrangement.spacedBy(Constants.Dimens.dp8),
                 verticalAlignment = Alignment.CenterVertically
-            ) {
-                languageIcon?.let { Text(it) }
-                languageName?.let { Text(it) }
-            }
+            ) { languageIcon?.let { Text(it) }; languageName?.let { Text(it) } }
         }
     )
-}
-
-@Composable
-@Preview
-private fun TestBtnConfLang() = Theme.AppTheme(
-    darkThemeEnabled = false,
-    dynamicColorEnabled = true,
-    contrastAccent = ContrastAccent.defaultValue,
-    fontSize = FontSize.defaultValue
-) {
-    Surface  {
-        Column(modifier = Modifier.fillMaxSize()) {
-            BtnConfLang(
-                languageIcon = String(Character.toChars(0x1F1EE))+String(Character.toChars(0x1F1E9)),
-                languageName = "Indonesia",
-                onClick = {}
-            )
-        }
-    }
 }
--- app/src/main/java/com/thomas200593/mdm/features/onboarding/ui/ScrOnboarding.kt
+++ app/src/main/java/com/thomas200593/mdm/features/onboarding/ui/ScrOnboarding.kt
@@ -1,18 +1,16 @@
 package com.thomas200593.mdm.features.onboarding.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.thomas200593.mdm.core.ui.common.Theme
+import com.thomas200593.mdm.core.ui.component.BtnConfLang
 import com.thomas200593.mdm.features.conf.__contrast_accent.entity.ContrastAccent
 import com.thomas200593.mdm.features.conf.__font_size.entity.FontSize
 
@@ -23,16 +21,37 @@ fun ScrOnboarding() {
         topBar = {
             TopAppBar(
                 title = {},
-                actions = { Text("Actions") }
+                actions = {
+                    BtnConfLang(
+                        onClick = {},
+                        languageIcon = String(Character.toChars(0x1F1EE)) +
+                                String(Character.toChars(0x1F1E9))
+                    )
+                }
             )
         },
         content = {
-            Surface(modifier = Modifier.padding(it)) {
-                Text("Onboarding Body")
+            Surface(modifier = Modifier.padding(it).fillMaxSize()) {
+                OnboardingImages()
+                OnboardingDetails()
+                OnboardingNavigation()
+                OnboardingTabSelector()
             }
         }
     )
 }
+
+@Composable
+private fun OnboardingImages() {}
+
+@Composable
+private fun OnboardingDetails() {}
+
+@Composable
+private fun OnboardingNavigation() {}
+
+@Composable
+private fun OnboardingTabSelector() {}
 
 @Composable
 @Preview
