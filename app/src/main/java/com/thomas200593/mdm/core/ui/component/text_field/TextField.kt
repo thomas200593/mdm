d5863ce749b32f386585084e600776734fbe8f2a
thomas200593 <thomas200593@yahoo.com>
2025-03-13 08:32:40 +0700

    U Upgrade to Kotlin 2.1.0

--- app/src/main/java/com/thomas200593/mdm/core/ui/component/text_field/TextField.kt
+++ app/src/main/java/com/thomas200593/mdm/core/ui/component/text_field/TextField.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.MaterialTheme
@@ -27,6 +29,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
+import com.thomas200593.mdm.core.ui.common.Theme
+import com.thomas200593.mdm.features.conf.__contrast_accent.entity.ContrastAccent
+import com.thomas200593.mdm.features.conf.__font_size.entity.FontSize
 import java.util.regex.Pattern
 
 @Composable
@@ -36,12 +41,13 @@ fun TxtFieldEmail(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     readOnly: Boolean = false,
+    inputTransformation: InputTransformation? = null,
     textStyle: TextStyle = MaterialTheme.typography.bodyLarge,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next),
     label: @Composable (() -> Unit)? = null,
     errorMessage: String? = null,
     scrollState: ScrollState = rememberScrollState(),
     cursorBrush: Brush = Brush.horizontalGradient(listOf(MaterialTheme.colorScheme.primary, MaterialTheme.colorScheme.primary)),
-    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next),
     onTextLayout: (Density.(() -> TextLayoutResult?) -> Unit)? = null,
 ) {
     var isError by remember { mutableStateOf(false) }
@@ -57,6 +63,7 @@ fun TxtFieldEmail(
             modifier = modifier,
             enabled = enabled,
             readOnly = readOnly,
+            inputTransformation = inputTransformation,
             textStyle = textStyle,
             keyboardOptions = keyboardOptions,
             lineLimits = TextFieldLineLimits.SingleLine,
@@ -107,29 +114,37 @@ private fun EmailTextFieldDecorator(
 
 @Preview(showBackground = true)
 @Composable
-fun PreviewTxtFieldEmail() {
-    var email by remember { mutableStateOf("") }
-    var errorMessage by remember { mutableStateOf<String?>(null) }
+fun PreviewTxtFieldEmail() = Theme.AppTheme(
+    darkThemeEnabled = true,
+    dynamicColorEnabled = false,
+    contrastAccent = ContrastAccent.defaultValue,
+    fontSize = FontSize.defaultValue
+) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            var email by remember { mutableStateOf("") }
+            var errorMessage by remember { mutableStateOf<String?>(null) }
 
-    val validateEmail = { email: String ->
-        val emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"
-        val pattern = Pattern.compile(emailRegex)
-        errorMessage = if (!pattern.matcher(email).matches() && email.isNotEmpty()) {
-            "Invalid email address"
-        } else {
-            null
+            val validateEmail = { email: String ->
+                val emailRegex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$"
+                val pattern = Pattern.compile(emailRegex)
+                errorMessage = if (!pattern.matcher(email).matches() && email.isNotEmpty()) {
+                    "Invalid email address"
+                } else {
+                    null
+                }
+            }
+            TxtFieldEmail(
+                value = email,
+                onValueChange = {
+                    email = it
+                    validateEmail(it)
+                },
+                label = { Text("Email") },
+                errorMessage = errorMessage
+            )
         }
-    }
-
-    Column(modifier = Modifier.padding(16.dp)) {
-        TxtFieldEmail(
-            value = email,
-            onValueChange = {
-                email = it
-                validateEmail(it)
-            },
-            label = { Text("Email") },
-            errorMessage = errorMessage
-        )
     }
 }
