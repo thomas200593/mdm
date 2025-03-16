14029f336831ffb6062c85248d626e07a53edf51
thomas200593 <thomas200593@yahoo.com>
2025-03-16 19:38:55 +0700

    U libs.versions.toml

--- app/src/main/java/com/thomas200593/mdm/features/initial/ui/VMInitial.kt
+++ app/src/main/java/com/thomas200593/mdm/features/initial/ui/VMInitial.kt
@@ -3,8 +3,8 @@ package com.thomas200593.mdm.features.initial.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.thomas200593.mdm.core.design_system.util.update
+import com.thomas200593.mdm.features.conf.common.entity.Common
 import com.thomas200593.mdm.features.initial.domain.UCGetDataInitial
-import com.thomas200593.mdm.features.initial.entity.Initial
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
@@ -14,27 +14,27 @@ import javax.inject.Inject
 class VMInitial @Inject constructor(
     private val ucGetDataInitial: UCGetDataInitial
 ) : ViewModel() {
-    sealed interface Ui {
-        data class Data(val dataState: DataState = DataState.Loading) : Ui
-        sealed interface DataState : Ui {
-            data object Loading : DataState
-            data class Loaded(val data: Initial) : DataState
-        }
-        sealed interface Events : Ui {
-            data object OnOpenEvent : Events
-        }
+    data class ScrData(
+        val confCommon: Common
+    )
+    sealed interface ScrDataState {
+        data object Loading : ScrDataState
+        data class Loaded(val scrData: ScrData) : ScrDataState
     }
-
-    var uiState = MutableStateFlow(Ui.Data())
+    data class UiState(
+        val scrDataState: ScrDataState = ScrDataState.Loading
+    )
+    sealed interface Events {
+        data object OnOpenEvent : Events
+    }
+    var uiState = MutableStateFlow(UiState())
         private set
-
-    fun onEvent(events : Ui.Events) {
-        when (events) {
-            Ui.Events.OnOpenEvent -> onOpenEvent()
+    fun onEvent(events: Events) {
+        when(events) {
+            is Events.OnOpenEvent -> onOpenEvent()
         }
     }
-
-    private fun onOpenEvent() = viewModelScope.launch {
-        ucGetDataInitial.invoke().collect { data -> uiState.update { it.copy(dataState = Ui.DataState.Loaded(data = data)) } }
+    private fun onOpenEvent() = viewModelScope.launch { ucGetDataInitial.invoke().collect { initial ->
+        uiState.update { state -> state.copy(scrDataState = ScrDataState.Loaded(scrData = ScrData(confCommon = initial.confCommon))) } }
     }
 }
