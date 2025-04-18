597c2f68228b559c0cf6af9c3623c67f25215e05
thomas200593 <thomas200593@yahoo.com>
2025-04-18 21:44:46 +0700

    U Auth Module

--- app/src/main/java/com/thomas200593/mdm/features/user/dao/DaoUser.kt
+++ app/src/main/java/com/thomas200593/mdm/features/user/dao/DaoUser.kt
@@ -6,6 +6,7 @@ import androidx.room.OnConflictStrategy.Companion.ABORT
 import androidx.room.Query
 import com.thomas200593.mdm.core.data.local.database.AppDatabase
 import com.thomas200593.mdm.features.user.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @Dao
@@ -13,10 +14,9 @@ interface DaoUser {
     @Insert(entity = UserEntity::class, onConflict = ABORT)
     suspend fun insertUser(user : UserEntity) : Long
     @Query("SELECT * FROM user WHERE email = :email LIMIT 1")
-    suspend fun getUserByEmail(email: String) : UserEntity?
+    fun getUserByEmail(email: String) : Flow<UserEntity?>
 }
-
 class DaoUserImpl @Inject constructor(private val appDatabase: AppDatabase) : DaoUser {
     override suspend fun insertUser(user: UserEntity): Long = appDatabase.daoUser().insertUser(user)
-    override suspend fun getUserByEmail(email: String): UserEntity? = appDatabase.daoUser().getUserByEmail(email)
+    override fun getUserByEmail(email: String): Flow<UserEntity?> = appDatabase.daoUser().getUserByEmail(email)
 }
--- app/src/main/java/com/thomas200593/mdm/features/user/repository/RepoUser.kt
+++ app/src/main/java/com/thomas200593/mdm/features/user/repository/RepoUser.kt
@@ -2,17 +2,17 @@ package com.thomas200593.mdm.features.user.repository
 
 import com.thomas200593.mdm.features.user.dao.DaoUser
 import com.thomas200593.mdm.features.user.entity.UserEntity
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 interface RepoUser {
     suspend fun getOrCreateUser(user: UserEntity) : Result<UserEntity>
 }
-
 class RepoUserImpl @Inject constructor(
     private val daoUser: DaoUser
 ) : RepoUser {
     override suspend fun getOrCreateUser(user: UserEntity) : Result<UserEntity> =
-        runCatching { daoUser.getUserByEmail(user.email) ?: user.takeIf { daoUser.insertUser(it) > 0 } ?: error("ErrorDialog creating user.") }
+        runCatching { daoUser.getUserByEmail(user.email).first() ?: user.takeIf { daoUser.insertUser(it) > 0 } ?: error("ErrorDialog creating user.") }
             .fold(
                 onSuccess = { entity -> Result.success(entity) },
                 onFailure = { t -> Result.failure(t) }
