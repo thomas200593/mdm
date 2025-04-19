6ead57d96b367c64ce939ea9256b5e6bb56245d9
thomas200593 <thomas200593@yahoo.com>
2025-04-19 15:03:12 +0700

    U RepoUser.kt fixing suspend to flow  U VMAuth.kt consume UCSignIn.kt

--- app/src/main/java/com/thomas200593/mdm/features/auth/domain/UCSignIn.kt
+++ app/src/main/java/com/thomas200593/mdm/features/auth/domain/UCSignIn.kt
@@ -2,13 +2,11 @@ package com.thomas200593.mdm.features.auth.domain
 
 import com.thomas200593.mdm.features.auth.entity.AuthType
 import com.thomas200593.mdm.features.auth.entity.DTOSignIn
-import com.thomas200593.mdm.features.user.repository.RepoUser
 import javax.inject.Inject
 
 class UCSignIn @Inject constructor(
-    private val repoUser: RepoUser
 ) {
     suspend operator fun invoke(dto: DTOSignIn) = when (dto.authType) {
-        is AuthType.LocalEmailPassword -> { val result = repoUser.getOneByEmail(dto.email) }
+        is AuthType.LocalEmailPassword -> {  }
     }
 }
