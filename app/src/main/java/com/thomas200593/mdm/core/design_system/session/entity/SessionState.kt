9866c7e02085535b70846d41f4198ebdc0b68a37
thomas200593 <thomas200593@yahoo.com>
2025-04-23 23:24:23 +0700

    U AuthEntity.kt update name  U DaoAuth.kt update name

--- app/src/main/java/com/thomas200593/mdm/core/design_system/session/entity/SessionState.kt
+++ app/src/main/java/com/thomas200593/mdm/core/design_system/session/entity/SessionState.kt
@@ -4,6 +4,6 @@ import com.thomas200593.mdm.features.user.entity.UserEntity
 
 sealed interface SessionState {
     data object Loading : SessionState
-    data class Valid(val session: Pair<SessionEntity, UserEntity>) : SessionState
+    data class Valid(val data: Pair<SessionEntity, UserEntity>) : SessionState
     data class Invalid(val t: Throwable) : SessionState
 }
--- app/src/main/java/com/thomas200593/mdm/features/initial/ui/ScrInitial.kt
+++ app/src/main/java/com/thomas200593/mdm/features/initial/ui/ScrInitial.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.thomas200593.mdm.app.main.nav.ScrGraphs
+import com.thomas200593.mdm.core.design_system.session.entity.SessionState
 import com.thomas200593.mdm.core.design_system.state_app.LocalStateApp
 import com.thomas200593.mdm.core.design_system.state_app.StateApp
 import com.thomas200593.mdm.core.ui.component.screen.ScrLoading
@@ -18,6 +19,7 @@ import com.thomas200593.mdm.features.initialization.nav.navToInitialization
 import com.thomas200593.mdm.features.onboarding.entity.OnboardingStatus
 import com.thomas200593.mdm.features.onboarding.nav.navToOnboarding
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 @Composable
@@ -28,7 +30,7 @@ fun ScrInitial(
     val uiState by vm.uiState.collectAsStateWithLifecycle()
     LaunchedEffect(key1 = Unit, block = { vm.onScreenEvent(Events.Screen.Opened) })
     ScrInitial(
-        scrGraph = scrGraph, componentsState = uiState.componentsState,
+        scrGraph = scrGraph, componentsState = uiState.componentsState, sessionState = stateApp.isSessionValid,
         onNavToOnboarding = { coroutineScope.launch { stateApp.navController.navToOnboarding() } },
         onNavToInitialization = { coroutineScope.launch { stateApp.navController.navToInitialization() } },
         onNavToAuth = { coroutineScope.launch { stateApp.navController.navToAuth() } }
@@ -36,25 +38,32 @@ fun ScrInitial(
 }
 @Composable
 private fun ScrInitial(
-    scrGraph: ScrGraphs.Initial, componentsState: ComponentsState, onNavToOnboarding: () -> Unit,
-    onNavToInitialization: () -> Unit, onNavToAuth: () -> Unit
+    scrGraph: ScrGraphs.Initial, componentsState: ComponentsState, sessionState: StateFlow<SessionState>,
+    onNavToOnboarding: () -> Unit, onNavToInitialization: () -> Unit, onNavToAuth: () -> Unit
 ) = when (componentsState) {
     is ComponentsState.Loading -> ScrLoading(label = scrGraph.title)
     is ComponentsState.Loaded -> ScreenContent(
-        components = componentsState,
-        onNavToOnboarding = onNavToOnboarding,
-        onNavToInitialization = onNavToInitialization,
-        onNavToAuth = onNavToAuth
+        components = componentsState, sessionState = sessionState,
+        onNavToOnboarding = onNavToOnboarding, onNavToInitialization = onNavToInitialization, onNavToAuth = onNavToAuth
     )
 }
 @Composable
 private fun ScreenContent(
-    components: ComponentsState.Loaded, onNavToOnboarding: () -> Unit, onNavToInitialization: () -> Unit,
-    onNavToAuth: () -> Unit
+    components: ComponentsState.Loaded, sessionState: StateFlow<SessionState>,
+    onNavToOnboarding: () -> Unit, onNavToInitialization: () -> Unit, onNavToAuth: () -> Unit
 ) = when (components.confCommon.onboardingStatus) {
     OnboardingStatus.SHOW -> onNavToOnboarding()
     OnboardingStatus.HIDE -> when (components.confCommon.firstTimeStatus) {
         FirstTimeStatus.YES -> onNavToInitialization()
-        FirstTimeStatus.NO -> onNavToAuth()
+        FirstTimeStatus.NO -> {
+            val session = sessionState.collectAsStateWithLifecycle().value
+            LaunchedEffect(session) {
+                when(session) {
+                    is SessionState.Loading -> Unit
+                    is SessionState.Invalid -> onNavToAuth()
+                    is SessionState.Valid -> {/*TODO*/}
+                }
+            }
+        }
     }
 }
