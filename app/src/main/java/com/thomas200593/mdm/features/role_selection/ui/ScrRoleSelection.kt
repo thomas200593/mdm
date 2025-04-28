6a5e2356a0fdf25cef6d7f96a9ef3dd632b3f882
thomas200593 <thomas200593@yahoo.com>
2025-04-28 22:38:03 +0700

    A Extract Session Handler and hold to stateapp

--- app/src/main/java/com/thomas200593/mdm/features/role_selection/ui/ScrRoleSelection.kt
+++ app/src/main/java/com/thomas200593/mdm/features/role_selection/ui/ScrRoleSelection.kt
@@ -3,10 +3,18 @@ package com.thomas200593.mdm.features.role_selection.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.thomas200593.mdm.R
 import com.thomas200593.mdm.app.main.nav.ScrGraphs
+import com.thomas200593.mdm.core.design_system.session.entity.SessionEvent
 import com.thomas200593.mdm.core.design_system.state_app.LocalStateApp
+import com.thomas200593.mdm.core.design_system.state_app.SessionHandler
 import com.thomas200593.mdm.core.design_system.state_app.StateApp
+import com.thomas200593.mdm.core.ui.component.dialog.LoadingDialog
+import com.thomas200593.mdm.core.ui.component.screen.ScrLoading
+import com.thomas200593.mdm.features.auth.nav.navToAuth
+import com.thomas200593.mdm.features.initial.nav.navToInitial
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable fun ScrRoleSelection(
     scrGraph: ScrGraphs.RoleSelection, vm: VMRoleSelection = hiltViewModel(), stateApp: StateApp = LocalStateApp.current,
