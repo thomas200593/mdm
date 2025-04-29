eb7047712ac8133c04551f1407b13af8dd8cd5bd
thomas200593 <thomas200593@yahoo.com>
2025-04-30 00:43:31 +0700

    A Extract Session Handler and hold to stateapp

--- app/src/main/java/com/thomas200593/mdm/features/role_selection/ui/VMRoleSelection.kt
+++ app/src/main/java/com/thomas200593/mdm/features/role_selection/ui/VMRoleSelection.kt
@@ -2,6 +2,7 @@ package com.thomas200593.mdm.features.role_selection.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.thomas200593.mdm.core.design_system.error.Error
 import com.thomas200593.mdm.core.design_system.session.entity.DTOSessionUserData
 import com.thomas200593.mdm.core.design_system.session.entity.SessionEvent
 import com.thomas200593.mdm.features.role_selection.domain.UCGetScreenData
@@ -12,6 +13,7 @@ import com.thomas200593.mdm.features.role_selection.ui.state.ResultGetUserRole
 import com.thomas200593.mdm.features.role_selection.ui.state.ResultSetUserRole
 import com.thomas200593.mdm.features.user_role.domain.UCGetUserRole
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
@@ -33,6 +35,15 @@ import javax.inject.Inject
         is SessionEvent.Invalid -> handleSessionInvalid(event, error)
         is SessionEvent.NoRole, is SessionEvent.Valid -> handleSessionValid(event, data)
     }
+    private inline fun updateUiState(crossinline transform: (ComponentsState.Loaded) -> ComponentsState) =
+        viewModelScope.launch(Dispatchers.Main.immediate) {
+            uiState.update { current ->
+                (current.componentsState as? ComponentsState.Loaded)
+                    ?. let(transform)
+                    ?. let { updatedState -> current.copy(componentsState = updatedState) }
+                    ?: current
+            }
+        }
     private fun handleOnOpenEvent() = viewModelScope.launch {
         ucGetScreenData.invoke()
             .onStart { uiState.update { it.copy(componentsState = ComponentsState.Loading) } }
@@ -47,7 +58,18 @@ import javax.inject.Inject
                 ) }
             }
     }
+    private fun updateDialog(transform: (DialogState) -> DialogState) = updateUiState { it.copy(dialogState = transform(it.dialogState)) }
     private fun handleSessionLoading(event: SessionEvent) {}
     private fun handleSessionInvalid(event: SessionEvent, throwable: Throwable?) {}
-    private fun handleSessionValid(event: SessionEvent, data: DTOSessionUserData?) {}
+    private fun handleSessionValid(event: SessionEvent, data: DTOSessionUserData?) {
+        updateUiState { it.copy(dialogState = DialogState.None, resultGetUserRole = ResultGetUserRole.Loading) }
+        data?.let { data ->
+            viewModelScope.launch {
+                ucGetUserRole.invoke(data.user).collect { result -> result
+                    .onSuccess { roles -> updateUiState { it.copy(resultGetUserRole = ResultGetUserRole.Success(roles)) } }
+                    .onFailure { err -> updateUiState { it.copy(resultGetUserRole = ResultGetUserRole.Error(err)) } }
+                }
+            } }
+            ?: updateUiState { it.copy(resultGetUserRole = ResultGetUserRole.Error(Error.Input.MalformedError(message = "Cannot get empty user"))) }
+    }
 }
