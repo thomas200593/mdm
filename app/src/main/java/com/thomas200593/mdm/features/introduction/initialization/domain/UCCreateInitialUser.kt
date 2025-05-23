e42f4435b4f6d90afc1c2dc4a928c0605304eba8
thomas200593 <thomas200593@yahoo.com>
2025-05-23 23:05:34 +0700

    U ScrAuth.kt

--- app/src/main/java/com/thomas200593/mdm/features/introduction/initialization/domain/UCCreateInitialUser.kt
+++ app/src/main/java/com/thomas200593/mdm/features/introduction/initialization/domain/UCCreateInitialUser.kt
@@ -26,39 +26,27 @@ class UCCreateInitialUser @Inject constructor(
     private val repoInitialization : RepoInitialization
 ) {
     suspend operator fun invoke(dto : DTOInitialization) : Result<DTOInitialization> {
-        if(dto.initialSetOfRoles.isEmpty()) return Result.failure(
-            Error.Input.MalformedError(
-                message = "Initial user cannot have no role(s)"
-            )
-        )
+        if(dto.initialSetOfRoles.isEmpty()) return Result
+            .failure(Error.Input.MalformedError(message = "Initial user cannot have no role(s)"))
         return repoUser.getOneByEmail(dto.email).flowOn(ioDispatcher).first().let {
             when {
-                it.isSuccess -> Result.failure(
-                    Error.Data.DuplicateError(
-                        message = "User with email ${dto.email} already exists"
-                    )
-                )
+                it.isSuccess -> Result
+                    .failure(Error.Data.DuplicateError(message = "User with email ${dto.email} already exists"))
                 it.isFailure -> {
                     val error = it.exceptionOrNull()
                     when (error) {
                         is Error.Database.DaoQueryNoDataError -> when(dto.authType) {
                             is AuthType.LocalEmailPassword -> {
                                 val input = dto.copy(
-                                    authType = dto.authType.copy(
-                                        password = bCrypt.hash(dto.authType.password)
-                                    )
+                                    authType = dto.authType
+                                        .copy(password = bCrypt.hash(dto.authType.password))
                                 )
                                 val user = input.toUserEntity(uid = UUIDv7.generateAsString())
                                 val auth = input.toAuthEntity(uid = user.uid)
                                 val profile = input.toUserProfileEntity(uid = user.uid)
-                                val roles = input.toUserRoleEntity(
-                                    uid = user.uid, roles = dto.initialSetOfRoles
-                                )
+                                val roles = input.toUserRoleEntity(uid = user.uid, roles = dto.initialSetOfRoles)
                                 repoInitialization.createUserLocalEmailPassword(
-                                    user = user,
-                                    auth = auth,
-                                    profile = profile,
-                                    roles = roles
+                                    user = user, auth = auth, profile = profile, roles = roles
                                 ).fold(
                                     onSuccess = {
                                         repoInitialization.updateFirstTimeStatus(FirstTimeStatus.NO)
@@ -68,16 +56,10 @@ class UCCreateInitialUser @Inject constructor(
                                 )
                             }
                         }
-                        is Error.Database.DaoQueryError -> Result.failure(
-                            Error.Database.DaoQueryError(
-                                cause = error.cause
-                            )
-                        )
-                        else -> Result.failure(
-                            Error.UnknownError(
-                                message = error?.message,
-                                cause = error?.cause
-                            )
+                        is Error.Database.DaoQueryError -> Result
+                            .failure(Error.Database.DaoQueryError(cause = error.cause))
+                        else -> Result
+                            .failure(Error.UnknownError(message = error?.message, cause = error?.cause)
                         )
                     }
                 }
