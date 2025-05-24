4e87e185f1123983ba0f539f6f20f9ac320cc79b
thomas200593 <thomas200593@yahoo.com>
2025-05-24 18:19:31 +0700

    U ScrAuth.kt

--- app/src/main/java/com/thomas200593/mdm/features/user_management/user_role/ui/state/DialogState.kt
+++ app/src/main/java/com/thomas200593/mdm/features/user_management/user_role/ui/state/DialogState.kt
@@ -3,5 +3,4 @@ package com.thomas200593.mdm.features.user_management.user_role.ui.state
 sealed interface DialogState {
     data object None : DialogState
     data object ScrDescDialog : DialogState
-    data class SessionInvalidDialog(val t : Throwable?) : DialogState
 }
---
+++ app/src/main/java/com/thomas200593/mdm/features/user_management/user_role/ui/state/ScreenDataState.kt
@@ -0,0 +1,15 @@
+package com.thomas200593.mdm.features.user_management.user_role.ui.state
+
+import com.thomas200593.mdm.features.user_management.security.session.entity.SessionEntity
+import com.thomas200593.mdm.features.user_management.security.session.entity.SessionEvent
+import com.thomas200593.mdm.features.common.cnf_common.entity.Common
+
+sealed interface ScreenDataState {
+    data object Loading : ScreenDataState
+    data class Loaded(
+        val confCommon : Common,
+        val sessionEvent : SessionEvent,
+        val sessionData : SessionEntity?,
+        val resultGetUserRole : ResultGetUserRole
+    ) : ScreenDataState
+}
