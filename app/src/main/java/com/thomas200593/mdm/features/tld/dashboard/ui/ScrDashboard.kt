b6246688c10c026fb17383ed028c04e01335b987
thomas200593 <thomas200593@yahoo.com>
2025-05-31 08:14:25 +0700

    U Update use case role selection

--- app/src/main/java/com/thomas200593/mdm/features/tld/dashboard/ui/ScrDashboard.kt
+++ app/src/main/java/com/thomas200593/mdm/features/tld/dashboard/ui/ScrDashboard.kt
@@ -1,11 +1,31 @@
 package com.thomas200593.mdm.features.tld.dashboard.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
 import com.thomas200593.mdm.app.main.nav.ScrGraphs
+import com.thomas200593.mdm.core.design_system.state_app.LocalStateApp
+import com.thomas200593.mdm.core.design_system.state_app.StateApp
+import com.thomas200593.mdm.features.auth.nav.navToAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
-fun ScrDashboard(scrGraph: ScrGraphs.DestTopLevel.Dashboard) {
-    Text("Screen : ${stringResource(scrGraph.title)}")
+fun ScrDashboard(
+    scrGraph: ScrGraphs.DestTopLevel.Dashboard,
+    stateApp: StateApp = LocalStateApp.current,
+    coroutineScope: CoroutineScope = rememberCoroutineScope()
+) {
+    Column {
+        Text("Screen : ${stringResource(scrGraph.title)}")
+        Button(
+            onClick = { coroutineScope.launch { stateApp.navController.navToAuth() } },
+            content = {
+                Text("Logout")
+            }
+        )
+    }
 }
