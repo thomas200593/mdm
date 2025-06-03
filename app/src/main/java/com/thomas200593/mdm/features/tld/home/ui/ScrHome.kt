bcdcd76335bc92872a72ad339a3fc32c40bec401
thomas200593 <thomas200593@yahoo.com>
2025-06-03 21:31:43 +0700

    U Refactor UiLoading.kt (Unification)

--- app/src/main/java/com/thomas200593/mdm/features/tld/home/ui/ScrHome.kt
+++ app/src/main/java/com/thomas200593/mdm/features/tld/home/ui/ScrHome.kt
@@ -1,28 +1,39 @@
 package com.thomas200593.mdm.features.tld.home.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.Modifier
 import com.thomas200593.mdm.app.main.nav.ScrGraphs
 import com.thomas200593.mdm.core.design_system.state_app.LocalStateApp
 import com.thomas200593.mdm.core.design_system.state_app.StateApp
-import com.thomas200593.mdm.features.auth.nav.navToAuth
+import com.thomas200593.mdm.core.design_system.util.Constants
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @Composable fun ScrHome(
     scrGraph: ScrGraphs.DestTopLevel.Home,
     stateApp: StateApp = LocalStateApp.current, coroutineScope: CoroutineScope = rememberCoroutineScope()
 ) {
-    ScrHome(
-        scrGraph = scrGraph
-    )
+    ScrHome()
 }
-@Composable private fun ScrHome(scrGraph: ScrGraphs.DestTopLevel.Home) {
-    Column {
-        Text("Screen : ${stringResource(scrGraph.title)}")
+@Composable private fun ScrHome() {
+    ScreenContent()
+}
+@Composable private fun ScreenContent() {
+    SectionContent()
+}
+@Composable private fun SectionContent() = Surface(
+    modifier = Modifier.fillMaxSize(),
+    content = {
+        LazyColumn (
+            modifier = Modifier.fillMaxSize(),
+            content = {
+                item { Text("Home page") }
+            }
+        )
     }
-}
+)
