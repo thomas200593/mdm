c608d6d9db257b2aed7c6b67b37f010856993d58
thomas200593 <thomas200593@yahoo.com>
2025-06-03 19:55:38 +0700

    U Refactor UiLoading.kt (Unification)

--- app/src/main/java/com/thomas200593/mdm/core/ui/component/permission/PermissionViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.thomas200593.mdm.core.ui.component.permission
-
-import androidx.lifecycle.ViewModel
-import com.thomas200593.mdm.core.ui.component.permission.repository.RepoPermissionHandler
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel class PermissionViewModel @Inject constructor(private val repoPermissionHandler: RepoPermissionHandler) : ViewModel()
--- app/src/main/java/com/thomas200593/mdm/core/ui/component/permission/entity/DeniedAppPermissions.kt
+++
@@ -1,5 +0,0 @@
-package com.thomas200593.mdm.core.ui.component.permission.entity
-
-data class DeniedAppPermissions(
-    val deniedPermissions : Set<String>
-)
