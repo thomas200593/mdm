753f88b05208a5809e93b2923b9d72d56f423240
thomas200593 <thomas200593@yahoo.com>
2025-06-05 19:52:10 +0700

    Reunify top app bar

---
+++ app/src/main/java/com/thomas200593/mdm/core/ui/common/anim/SlideDownFadeAnim.kt
@@ -0,0 +1,28 @@
+package com.thomas200593.mdm.core.ui.common.anim
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+private val slideFadeIn: EnterTransition = fadeIn() + slideInVertically()
+private val slideFadeOut: ExitTransition = fadeOut() + slideOutVertically()
+@Composable fun SlideDownFadeAnim(
+    modifier : Modifier = Modifier,
+    visible : Boolean,
+    label : String = "AnimatedVisibility",
+    content : @Composable AnimatedVisibilityScope.() -> Unit
+) = AnimatedVisibility(
+    modifier = modifier,
+    visible = visible,
+    enter = slideFadeIn,
+    exit = slideFadeOut,
+    label = label,
+    content = content
+)
--- app/src/main/java/com/thomas200593/mdm/features/tld/home/ui/ScrHome.kt
+++ app/src/main/java/com/thomas200593/mdm/features/tld/home/ui/ScrHome.kt
@@ -1,39 +1,125 @@
 package com.thomas200593.mdm.features.tld.home.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.SubcomposeAsyncImage
 import com.thomas200593.mdm.app.main.nav.ScrGraphs
 import com.thomas200593.mdm.core.design_system.state_app.LocalStateApp
 import com.thomas200593.mdm.core.design_system.state_app.StateApp
 import com.thomas200593.mdm.core.design_system.util.Constants
+import com.thomas200593.mdm.core.ui.common.anim.SlideDownFadeAnim
+import com.thomas200593.mdm.core.ui.component.text.TextType
+import com.thomas200593.mdm.core.ui.component.text.UiText
 import kotlinx.coroutines.CoroutineScope
 
 @Composable fun ScrHome(
     scrGraph: ScrGraphs.DestTopLevel.Home,
     stateApp: StateApp = LocalStateApp.current, coroutineScope: CoroutineScope = rememberCoroutineScope()
-) {
-    ScrHome()
-}
-@Composable private fun ScrHome() {
-    ScreenContent()
-}
-@Composable private fun ScreenContent() {
-    SectionContent()
-}
+) { ScrHome() }
+@Composable private fun ScrHome() { ScreenContent() }
+@Composable private fun ScreenContent() { SectionContent() }
 @Composable private fun SectionContent() = Surface(
     modifier = Modifier.fillMaxSize(),
     content = {
         LazyColumn (
             modifier = Modifier.fillMaxSize(),
             content = {
-                item { Text("Home page") }
+                item { SectionHeadline() }
             }
         )
     }
 )
+@OptIn(ExperimentalMaterial3ExpressiveApi::class) @Composable private fun SectionHeadline() {
+    var expanded by remember { mutableStateOf(false) }
+    val imgModel = null //temporary
+    Card(
+        modifier = Modifier.fillMaxWidth(), shape = RectangleShape,
+        content = {
+            Column (
+                content = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth().padding(Constants.Dimens.dp8),
+                        horizontalArrangement = Arrangement.spacedBy(Constants.Dimens.dp8, Alignment.CenterHorizontally),
+                        verticalAlignment = Alignment.CenterVertically,
+                        content = {
+                            Surface(
+                                shape = RoundedCornerShape(Constants.Dimens.dp8),
+                                border = BorderStroke(width = Constants.Dimens.dp1, color = MaterialTheme.colorScheme.outline),
+                                tonalElevation = Constants.Dimens.dp2,
+                                modifier = Modifier.size(Constants.Dimens.dp48),
+                                content = {
+                                    SubcomposeAsyncImage(
+                                        model = imgModel, contentDescription = null,
+                                        loading = { CircularProgressIndicator(modifier = Modifier.padding(Constants.Dimens.dp8)) },
+                                        contentScale = ContentScale.Crop, modifier = Modifier.fillMaxSize()
+                                    )
+                                }
+                            )
+                            UiText(
+                                text = "User Name", type = TextType.TITLE_MD, modifier = Modifier.weight(1f),
+                                maxLines = 1, overflow = TextOverflow.Ellipsis
+                            )
+                            IconButton (
+                                onClick = { expanded = expanded.not() },
+                                content = {
+                                    if(expanded) Icon(Icons.Default.ExpandLess, null)
+                                    else Icon(Icons.Default.ExpandMore, null)
+                                }
+                            )
+                        }
+                    )
+                    SlideDownFadeAnim(
+                        visible = expanded,
+                        content = {
+                            Column {
+                                HorizontalDivider()
+                                Row (modifier = Modifier.fillMaxWidth().padding(Constants.Dimens.dp8)) {
+                                    Column {
+                                        //Icons
+                                        UiText("Icons")
+                                    }
+                                    Column {
+                                        //Details
+                                        UiText("Details")
+                                    }
+                                }
+                            }
+                        }
+                    )
+                }
+            )
+        }
+    )
+}
+@Preview @Composable private fun Preview() = SectionContent()
